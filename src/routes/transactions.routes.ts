262a90f428adae8ef76e9eb69a65fbec0b61aa8b
Douglas Scriptore <douglass@rpc.com.br>
2020-08-27 14:33:06 -0300

    migrated from bootcamp_desafio_03

---
+++ src/routes/transactions.routes.ts
@@ -0,0 +1,60 @@
+import { Router } from 'express';
+import multer from 'multer';
+import { getCustomRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+
+import uploadConfig from '../config/upload';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+const upload = multer(uploadConfig);
+
+const transactionsRouter = Router();
+
+transactionsRouter.get('/', async (request, response) => {
+  const transactionRepository = getCustomRepository(TransactionsRepository);
+  const transactions = await transactionRepository.find();
+  const balance = await transactionRepository.getBalance();
+
+  return response.json({ transactions, balance });
+});
+
+transactionsRouter.post('/', async (request, response) => {
+  const { title, value, type, category } = request.body;
+
+  const createTransaction = new CreateTransactionService();
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
+});
+transactionsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  await deleteTransaction.execute(id);
+
+  return response.status(204).send();
+});
+
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    // TODO
+    const importTransaction = new ImportTransactionsService();
+
+    const transactions = await importTransaction.execute(request.file.path);
+
+    return response.json(transactions);
+  },
+);
+
+export default transactionsRouter;
---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,57 @@
+import { getRepository, getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+class CreateTransactionService {
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const categoriesRepository = getRepository(Category);
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance');
+    }
+
+    // find category
+    let transactionCategory = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoriesRepository.create({
+        title: category,
+      });
+      await categoriesRepository.save(transactionCategory);
+    }
+
+    // create transaction
+    const transaction = transactionsRepository.create({
+      title,
+      type,
+      value,
+      category: transactionCategory,
+    });
+    await transactionsRepository.save(transaction);
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
